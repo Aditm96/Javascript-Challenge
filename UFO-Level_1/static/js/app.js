d09ee2f9f7ee32ae33d9b779234ce4648dd03404
Adit <adit.mahmood1@gmail.com>
2020-07-07 14:05:47 -0400

    adding UFO-Level_1

---
+++ UFO-Level_1/static/js/app.js
@@ -0,0 +1,18 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+console.log(data)
+data.forEach(function(SpaceData) {
+    console.log(SpaceData);
+    var row = tbody.append("tr")
+
+    Object.entries(SpaceData).forEach(function([key, value]) {
+        console.log(key, value);
+
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
+
